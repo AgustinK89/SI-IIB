072858cb7a11e1d51b7c601d7ab4153109178f92
AgustinK89 <101956471+AgustinK89@users.noreply.github.com>
2022-03-24 11:08:17 +0700

    Add files via upload

---
+++ A3.2000005/Praktek 1.py
@@ -0,0 +1,8 @@
+print("Masukan panjang")
+panjang = int(input())
+print("Masukan Lebar")
+lebar = int(input())
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+print("luas persegi panjang adalah :" + str(luas))
+print("keliling persegi panjang adalah :" + str(keliling))
